b2a6641e9df2824008bc62e8f617359160795713
Artem Danko <vpeta4141@gmail.com>
2023-03-12 23:44:12 +0200

    feat: add post module

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ab-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css'],
+})
+export class PostComponent {}
